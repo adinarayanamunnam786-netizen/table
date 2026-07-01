bd40424e0d8eb13624a05bb181db0ed9aa14b571
adinarayanamunnam786-netizen <adinarayanamunnam786@gmail.com>
2026-07-01 19:28:25 +0530

    first commit

---
+++ set.py
@@ -0,0 +1,25 @@
+# Python Notes: List, Nested List, Tuple, Set, Dictionary
+
+# List (Ordered, Mutable, Allows Duplicates)
+fruits = ["Apple", "Banana", "Mango"]
+print("List:", fruits)
+
+# Nested List (List inside another list)
+data = [[1, 2, 3], [4, 5, 6]]
+print("Nested List:", data)
+
+# Tuple (Ordered, Immutable, Allows Duplicates)
+colors = ("Red", "Green", "Blue")
+print("Tuple:", colors)
+
+# Set (Unordered, Mutable, No Duplicates)
+numbers = {10, 20, 30, 20}
+print("Set:", numbers)
+
+# Dictionary (Key-Value Pairs, Mutable, Unique Keys)
+student = {
+    "Name": "Ravi",
+    "Age": 20,
+    "Course": "Python"
+}
+print("Dictionary:", student)
